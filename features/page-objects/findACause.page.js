7dd3031b626923023b46812096059824bc02af1e
Gurjit Sangha <gsangha494@gmail.com>
2022-11-03 10:10:54 +0000

    init

--- features/page-objects/findACause.page.js
+++ features/page-objects/findACause.page.js
@@ -2,17 +2,26 @@ const Page = require('./page');
 
 class FindACausePage extends Page {
   get searchInput() {
-    return $('#sagc-hero-search-input')
+    return $('#sagc-hero-search-input');
+  }
+
+  get searchResults() {
+    return $('#sagc-hero-search-input-auto-suggest');
+  }
+
+  get submitButton() {
+    return $('#sagc-hero-search-submit');
   }
 
   async fillInSearchInput(cause) {
-    await this.searchInput.click()
-    await this.searchInput.setValue(cause)
+    await this.searchInput.click();
+    await this.searchInput.setValue(cause);
   }
 
-  open () {
-    return super.open('/support-a-good-cause');
+  async selectCause(number) {
+    await this.searchResults.$$('li')[number - 1].click();
+    await this.submitButton.click();
   }
-} 
+}
 
 module.exports = new FindACausePage();
--- features/simple-cause-search.feature
+++ features/simple-cause-search.feature
@@ -1,13 +1,9 @@
 Feature: Simple cause search
-  
+
 
   Scenario: Search for third cause
     Given I open the Easy Fundraising website
     When I click on the Find A Cause link
-    When I enter can in the search field
-    # Then a list of matching causes will appear
-    # When I click the third cause
-    # Then it will appear in the search field
-    # When I click the Search Cause button
-    # Then the search results page will load 
-    # Then the cause will be in the search results
+    When I enter 'can' in the search field
+    When I select the cause number 3 in the results
+    Then I should see 'Marie Curie Cancer Care' in the search results
